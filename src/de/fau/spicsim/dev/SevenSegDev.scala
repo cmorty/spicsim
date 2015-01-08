64752d92f91327c8f1e87d1a731d96f0d3decb3b
Moritz 'Morty' Strübe <morty@gmx.net>
2015-01-08 18:00:21 +0100

    Formatting

--- src/de/fau/spicsim/dev/SevenSegDev.scala
+++ src/de/fau/spicsim/dev/SevenSegDev.scala
@@ -13,7 +13,7 @@ class SevenSegDev(ssdu: SpicSimDevUpdater) extends SpicSimDev(ssdu) with DevObse
 	//Config
 
 	private def bs(reg: Byte, bit: Int) = Arithmetic.getBit(reg, bit)
-	
+
 	val segMap = List(
 		3, // 0
 		4, // 1
@@ -21,10 +21,8 @@ class SevenSegDev(ssdu: SpicSimDevUpdater) extends SpicSimDev(ssdu) with DevObse
 		5, // 3
 		0, // 4
 		1, // 5
-		2  // 6
+		2 // 6
 	)
-	
-	
 
 	val segc = List.fill(2)(List.fill(7)(new PwmMon(this)))
 
@@ -33,20 +31,19 @@ class SevenSegDev(ssdu: SpicSimDevUpdater) extends SpicSimDev(ssdu) with DevObse
 	}
 	import scala.language.reflectiveCalls
 	val sMon = new Simulator.Watch.Empty {
-		
+
 		def getport(port: String) = interp.getRegisterByte(mcu.getProperties.getIORegAddr(port))
 		def regWatch(port: String) = sim.insertWatch(this, mcu.getProperties.getIORegAddr(port))
-		
+
 		override def fireAfterWrite(state: State, data_addr: Int, value: Byte) {
 			val portd = getport("PORTD")
 			val ddrd = getport("DDRD")
-			
+
 			val portb = getport("PORTB")
 			val ddrb = getport("DDRB")
-			
 
 			for (sSegId <- 0 to 1) {
-				if(bs(ddrd, sSegId) && !bs(portd, sSegId)) { //Output & High
+				if (bs(ddrd, sSegId) && !bs(portd, sSegId)) { //Output & High
 					for (seg <- 0 to 6) {
 						setSeg(sSegId, segMap(seg), bs(ddrb, seg) && !bs(portb, seg)) //Output & Low
 					}
@@ -65,7 +62,7 @@ class SevenSegDev(ssdu: SpicSimDevUpdater) extends SpicSimDev(ssdu) with DevObse
 		sMon.regWatch("DDRB")
 	}
 
-	def setSeg(sSegId: Int, seg: Int, on: Boolean){
+	def setSeg(sSegId: Int, seg: Int, on: Boolean) {
 		segc(sSegId)(seg).setLevel(on)
 	}
 
